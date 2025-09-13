8d9a8e5f1e1a3212b5dfcd6c998797e4aef73572
unknown <berriot.cesar.com>
2025-09-13 19:31:10 +0200

    added support for Module-Definition files

--- discover_sources.cmake
+++ discover_sources.cmake
@@ -1,4 +1,4 @@
 function(discover_sources target)
-  file(GLOB_RECURSE sources CONFIGURE_DEPENDS source/*.c source/*.cpp)
+  file(GLOB_RECURSE sources CONFIGURE_DEPENDS source/*.c source/*.cpp source/*.def)
   target_sources(${target} PRIVATE ${sources})
 endfunction()
